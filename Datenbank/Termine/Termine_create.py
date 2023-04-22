ce861b8d424307ac3d41198ff809e1aa4a7526ee
LeonardK2020 <leonard.muenchen@gmail.com>
2023-04-22 18:07:34 +0200

    termine.db fehler behoben

--- Datenbank/Termine/Termine_create.py
+++ Datenbank/Termine/Termine_create.py
@@ -2,7 +2,7 @@ import sqlite3
 
 connection = sqlite3.connect('termine.db')
 
-sql_create = "Create table termine (termin_id INTEGER PRIMARY KEY AUTOINCREMENT, datum_tag INTEGER NOT NULL, datum_monat INTEGER NOT NULL, datum_jahr INTEGER NOT NULL, wochentag TEXT NOT NULL, name TEXT NOT NULL, uhrzeit_anfang TEXT NOT NULL, uhrzeit_anfang TEXT NOT NULL)"
+sql_create = "Create table termine (termin_id INTEGER PRIMARY KEY AUTOINCREMENT, datum_tag INTEGER NOT NULL, datum_monat INTEGER NOT NULL, datum_jahr INTEGER NOT NULL, wochentag TEXT NOT NULL, name TEXT NOT NULL, uhrzeit_anfang TEXT, uhrzeit_ende TEXT)"
 
 
 connection.execute(sql_create)
