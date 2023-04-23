d8db49ba477d82e902eebf98ecb57fb691dac830
LeonardK2020 <leonard.muenchen@gmail.com>
2023-04-23 14:51:07 +0200

    priorität geändert

--- Datenbank/Aufgaben/Aufgaben_create.py
+++ Datenbank/Aufgaben/Aufgaben_create.py
@@ -2,7 +2,7 @@ import sqlite3
 
 connection = sqlite3.connect('aufgaben.db')
 
-sql_create = "Create table aufgaben (aufgaben_id INTEGER PRIMARY KEY AUTOINCREMENT, wochentag TEXT , name TEXT NOT NULL, uhrzeit TEXT , datum_abgabe_tag TEXT NOT NULL, datum_abgabe_monat TEXT NOT NULL, priorität TEXT NOT NULL)"
+sql_create = "Create table aufgaben (aufgaben_id INTEGER PRIMARY KEY AUTOINCREMENT, wochentag TEXT , name TEXT NOT NULL, uhrzeit TEXT , datum_abgabe_tag TEXT NOT NULL, datum_abgabe_monat TEXT NOT NULL, priorität TEXT )"
 
 
 connection.execute(sql_create)
