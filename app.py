37aaf69b5945cc910b7b12a96150bc68c73e09ae
ToniTown1010 <tonitown1010@gmail.com>
2023-05-10 09:09:27 +0200

    dateien umbenannt

--- app.py
+++ app.py
@@ -4,8 +4,8 @@ from flask_bootstrap import Bootstrap
 import time
 import flask_login
 import flask
-from modulefinder import RegForm #auf model.py anpassen
-
+from module import RegForm #auf model.py anpassen
+import module
 
 app = Flask(__name__)
 
@@ -15,7 +15,7 @@ app.config.from_mapping(
     SECRET_KEY=b'\xd6\x04\xbdj\xfe\xed$c\x1e@\xad\x0f\x13,@G')
 @app.route('/', methods=['GET', 'POST'])
 def registration():
-    form = RegForm(request.form)
+    form = module.RegForm(request.form)      #model davorgeschrieben
     if request.method == 'POST' and form.validate_on_submit():
         return 'Registrierung bestätigt!'
     return render_template('registration.html', form=form)
