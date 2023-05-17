d00d108114b658c741f5e74ca37384ffb7925f79
FrankfurtamA <leonard.muenchen@gmail.com>
2023-05-17 08:30:13 +0200

    1

--- app.py
+++ app.py
@@ -4,22 +4,22 @@ from flask_bootstrap import Bootstrap
 import time
 import flask_login
 import flask
-from module import RegForm #auf model.py anpassen
-import module
-from wtforms import validators
+#from module import RegForm #auf model.py anpassen
+#import module
+#from wtforms import validators
 
 app = Flask(__name__)
 
 Bootstrap = Bootstrap(app)
 
-app.config.from_mapping(
-    SECRET_KEY=b'\xd6\x04\xbdj\xfe\xed$c\x1e@\xad\x0f\x13,@G')
+#app.config.from_mapping(
+#    SECRET_KEY=b'\xd6\x04\xbdj\xfe\xed$c\x1e@\xad\x0f\x13,@G')
 @app.route('/', methods=['GET', 'POST'])
-def registration():
-    form = module.RegForm(request.form)      #model davorgeschrieben
-    if request.method == 'POST' and form.validate_on_submit():
-        return 'Registrierung bestätigt!'
-    return render_template('registration.html', form=form)
+#def registration():
+ #   form = module.RegForm(request.form)      #model davorgeschrieben
+  #  if request.method == 'POST' and form.validate_on_submit():
+   #     return 'Registrierung bestätigt!'
+    #return render_template('registration.html', form=form)
 
 @app.route("/index")
 def startseite():
