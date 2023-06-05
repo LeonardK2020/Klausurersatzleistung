d519d60a2d9436f478fd5d8837b682fe74ce1dda
ToniTown1010 <antonio.nemes@stu.kls-berlin.de>
2023-06-05 20:00:16 +0200

    änderung benutzer hinzufügen.py

--- Datenbank/benutzer hinzufügen.py
+++ Datenbank/benutzer hinzufügen.py
@@ -3,7 +3,7 @@ import sqlite3
 connection = sqlite3.connect('datenbank.db')
 #insert="INSERT INTO benutzer(vorname, nachname, benutzername, passwort) VALUES('Antonio', 'Nemes', 'antonio', 'antoniotest')"
 #insert="INSERT INTO benutzer(vorname, nachname, benutzername, passwort) VALUES('Lino', 'Lustig', 'lino', 'linotest')"
-#insert="INSERT INTO benutzer(vorname, nachname, benutzername, passwort) VALUES('test1', 'fehler1', 'a', 'test1')"
+insert="INSERT INTO benutzer(vorname, nachname, benutzername, passwort) VALUES('test1', 'fehler1', 'a', 'test1')"
 
 connection.execute(insert)
 connection.commit()
