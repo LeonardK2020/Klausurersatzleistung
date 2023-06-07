c3eb4b518c13f5060936e03992bf19245757cbc4
FrankfurtamA <leonard.muenchen@gmail.com>
2023-06-07 08:38:46 +0200

    a

--- Datenbank/datenbank_create.py
+++ Datenbank/datenbank_create.py
@@ -2,7 +2,7 @@ import sqlite3
 
 connection = sqlite3.connect('datenbank.db')
 
-sql_create1 = "Create table aufgaben  (aufgaben_id INTEGER PRIMARY KEY AUTOINCREMENT, wochentag TEXT ,  name TEXT, uhrzeit TEXT ,  datum_abgabe_tag TEXT , datum_abgabe_monat TEXT, priorität TEXT, benutzer_id INTEGER ,FOREIGN KEY(benutzer_id) REFERENCES benutzer(benutzer_id) )" 
+sql_create1 = "Create table aufgaben  (aufgaben_id INTEGER PRIMARY KEY AUTOINCREMENT, datum TEXT ,  name TEXT, uhrzeit TEXT ,  datum_abgabe_tag TEXT , datum_abgabe_monat TEXT, priorität TEXT, benutzer_id INTEGER ,FOREIGN KEY(benutzer_id) REFERENCES benutzer(benutzer_id) )" 
 sql_create2 = "Create table benutzer (benutzer_id INTEGER PRIMARY KEY AUTOINCREMENT, vorname TEXT , nachname TEXT, benutzername TEXT , passwort TEXT )" 
 sql_create3 = "Create table termine (termin_id INTEGER PRIMARY KEY AUTOINCREMENT, datum_tag INTEGER , datum_monat INTEGER , datum_jahr INTEGER , wochentag TEXT , name TEXT , uhrzeit_anfang TEXT, uhrzeit_ende TEXT, benutzer_id INTEGER ,FOREIGN KEY(benutzer_id) REFERENCES benutzer(benutzer_id) )"
 
